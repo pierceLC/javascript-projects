41ca619498470321e56c88a01f6e92f41b4c9380
pierceLC <pierceford933@gmail.com>
2024-01-31 05:37:52 -0600

    objects&math chap

--- objects-and-math/chapter-examples/KindnessSelection.js
+++ objects-and-math/chapter-examples/KindnessSelection.js
@@ -9,9 +9,27 @@ function randomSelection(arr){
  
  for (i=0; i < 8; i++){
    console.log(randomSelection(happiness));
+   
  }
- 
+ console.log(`-----------`);
  //Experiment with the code above. Try to:
  //a) Print 3 random selections from each array.
+ for (let i = 0; i < 3; i++){
+  console.log(randomSelection(happiness));
+}
+for (let i = 0; i < 3; i++){
+  console.log(randomSelection(words));
+}
+console.log(`-----------`);
  //b) Have the code randomly pick one array, and then print 2 random items from it.
+ let selectedArray = Math.random() < 0.5 ? happiness : words;
+ for (let i = 0; i < 2; i++){
+  console.log(randomSelection(selectedArray));
+}
+console.log(`-----------`);
  //c) Create a new array, then fill it with one random item from words and happiness. Print the new array.
+ let newArray = [];
+  newArray.push(randomSelection(happiness));
+  newArray.push(randomSelection(words));
+
+console.log(newArray);
